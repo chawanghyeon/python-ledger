5f75b1fc58419718da70e5d242e155d5950ce34a
chawanghyeon <chawanghyeon@gmail.com>
2023-04-04 00:24:07 +0900

    추가: serializers.py in users app

---
+++ users/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import CharField, ModelSerializer
+
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+    password = CharField(write_only=True)
+
+    class Meta:
+        fields = "__all__"
+        model = User
+        depth = 1
