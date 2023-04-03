ef7f6f4882bfc531833d612d4e59e371335cae5e
chawanghyeon <chawanghyeon@gmail.com>
2023-04-03 19:24:49 +0900

    추가: User model in models.py

--- users/models.py
+++ users/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    id = models.BigAutoField(primary_key=True)
+    username = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
