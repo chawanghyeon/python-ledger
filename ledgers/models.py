c9923c42bafe4b675d3b5dd8008df07f12d2cb27
chawanghyeon <chawanghyeon@gmail.com>
2023-04-04 00:19:31 +0900

    추가: custom_types app 수정: type field in Ledger model

--- ledgers/models.py
+++ ledgers/models.py
@@ -1,25 +1,21 @@
 from django.db import models
 
+from custom_types.models import CustomType
 from users.models import User
 
 
 class Ledger(models.Model):
-    TYPE_CHOICES = [
-        ("ED", "Education"),
-        ("FO", "Food"),
-        ("HO", "Housing"),
-        ("CO", "Communication"),
-        ("EN", "Entertainment"),
-        ("EX", "Extra"),
-    ]
-
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     memo = models.CharField(max_length=100, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    type = models.ForeignKey(CustomType, on_delete=models.PROTECT)
     date = models.DateField()
+
+    class Meta:
+        verbose_name_plural = "Ledgers"
+        ordering = ["-date", "-id"]
 
     def __str__(self):
         return self.name
