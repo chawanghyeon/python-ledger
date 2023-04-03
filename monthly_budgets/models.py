58649fe3647f3b3dbd7c96b1a00ffa35a28526ba
chawanghyeon <chawanghyeon@gmail.com>
2023-04-03 23:33:04 +0900

    추가: monthly_budgets app, MonthlyBudget model in models.py

---
+++ monthly_budgets/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from users.models import User
+
+
+class MonthlyBudget(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        unique_together = ("user", "year", "month")
+
+    def __str__(self):
+        return f"{self.year}-{self.month} Budget for user {self.user_id}"
