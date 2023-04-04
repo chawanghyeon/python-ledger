bb02ec5aede521141949847e5c7bd1446196567f
chawanghyeon <chawanghyeon@gmail.com>
2023-04-04 11:45:36 +0900

    추가: MonthlyBudgetViewSet

--- monthly_budgets/views.py
+++ monthly_budgets/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from rest_framework import viewsets
+
+from monthly_budgets.models import MonthlyBudget
+from monthly_budgets.serializers import MonthlyBudgetSerializer
+
+
+class MonthlyBudgetViewSet(viewsets.ModelViewSet):
+    queryset = MonthlyBudget.objects.all()
+    serializer_class = MonthlyBudgetSerializer
