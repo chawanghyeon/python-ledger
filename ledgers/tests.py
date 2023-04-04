1519e0a414fa9167e173b79f56ce27d6b23e216f
chawanghyeon <chawanghyeon@gmail.com>
2023-04-04 16:32:13 +0900

    추가: tests.py

--- ledgers/tests.py
+++ ledgers/tests.py
@@ -1,1 +1,16 @@
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.models import User
+
+
+class LedgerViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            username="user1@user1.com", password="user1_password"
+        )
+
+        self.user1_token = RefreshToken.for_user(self.user1)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Bearer {self.user1_token.access_token}"
+        )
