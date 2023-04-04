63b710f83be35545d8a0765f69a9445d5206e54a
chawanghyeon <chawanghyeon@gmail.com>
2023-04-04 16:45:55 +0900

    추가: create(), test_create_ledger()

--- ledgers/tests.py
+++ ledgers/tests.py
@@ -1,6 +1,10 @@
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from custom_types.models import CustomType
+from ledgers.models import Ledger
 from users.models import User
 
 
@@ -14,3 +18,25 @@ class LedgerViewSetTestCase(APITestCase):
         self.client.credentials(
             HTTP_AUTHORIZATION=f"Bearer {self.user1_token.access_token}"
         )
+
+        self.type1 = CustomType.objects.create(user=self.user1, name="type1")
+
+        self.ledger_data = {
+            "user_id": self.user1.id,
+            "name": "ledger1",
+            "memo": "memo1",
+            "amount": 1000,
+            "type_id": self.type1.id,
+            "date": "2023-04-04",
+        }
+
+        self.ledger1 = Ledger.objects.create(**self.ledger_data)
+
+    def test_create_ledger(self):
+        Ledger.objects.all().delete()
+        response = self.client.post(
+            reverse("ledgers-list"), self.ledger_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Ledger.objects.count(), 1)
+        self.assertEqual(response.data["name"], "ledger1")
--- ledgers/views.py
+++ ledgers/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.http import HttpRequest
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from ledgers.models import Ledger
 from ledgers.serializers import LedgerSerializer
@@ -7,3 +9,10 @@ from ledgers.serializers import LedgerSerializer
 class LedgerViewSet(viewsets.ModelViewSet):
     serializer_class = LedgerSerializer
     queryset = Ledger.objects.all()
+
+    def create(self, request: HttpRequest) -> Response:
+        serializer = LedgerSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user, type_id=request.data["type_id"])
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
