b3d3b8a2677447db9b1a96fb76ea31ec6e43c2a9
chawanghyeon <chawanghyeon@gmail.com>
2023-04-04 15:05:22 +0900

    추가: test_create_monthly_budget_without_authentication()

--- monthly_budgets/tests.py
+++ monthly_budgets/tests.py
@@ -106,3 +106,12 @@ class MonthlyBudgetViewSetTestCase(APITestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertFalse(MonthlyBudget.objects.exists())
+
+    def test_create_monthly_budget_without_authentication(self):
+        MonthlyBudget.objects.all().delete()
+        self.client.credentials()
+        response = self.client.post(
+            reverse("monthly-budgets-list"), self.monthly_budget_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertFalse(MonthlyBudget.objects.exists())
