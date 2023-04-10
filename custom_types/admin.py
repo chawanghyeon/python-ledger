564b250483419c94d728e3857ed6afc35f5ba357
chawanghyeon <chawanghyeon@gmail.com>
2023-04-10 23:08:05 +0900

    추가: 어드민 등록

--- custom_types/admin.py
+++ custom_types/admin.py
@@ -1,1 +1,5 @@
-# Register your models here.
+from django.contrib import admin
+
+from custom_types.models import CustomType
+
+admin.site.register(CustomType)
--- monthly_budgets/admin.py
+++ monthly_budgets/admin.py
@@ -1,1 +1,5 @@
-# Register your models here.
+from django.contrib import admin
+
+from monthly_budgets.models import MonthlyBudget
+
+admin.site.register(MonthlyBudget)
